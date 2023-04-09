1ab07bdb8056bbb808986b704e1d3b42101b7719
yusancky <yusancky@hotmail.com>
2023-04-09 08:14:42 +0000

    Create action.yml & index.js

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# setup-typst
+# Setup Typst
+
 Setup Typst in GitHub Actions
+
+## Inputs
+
+### `token`
+
+The token used to authenticate when fetching Typst distributions. When running this action on github.com, the default value is sufficient. When running on GHES, you can pass a personal access token for github.com if you are experiencing rate limiting.
+
+### `version`
+
+Exact version of Typst to use. Uses the latest if unset.
+
+## Outputs
+
+### `path`
+
+The absolute path to the Typst executable.
+
+## Example usage
+
+```yaml
+- uses: yusancky/setup-typst@v0.0.1
+  id: setup-typst
+  with:
+    version: '0.1.0'
+- run: ${{ steps.setup-typst.outputs.path }} compile file.typ
+```
+
+You can also use the additional uses given in [Usage](https://github.com/typst/typst#usage) in the Typst documentation.
---
+++ index.js
@@ -0,0 +1,55 @@
+const core = require('@actions/core');
+const { exec } = require('child_process');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
+const token = core.getInput('token');
+
+const versionFilePath = path.join(__dirname, 'version.json');
+let version;
+
+if (core.getInput('version')) {
+  version = core.getInput('version');
+  console.log(`Using version from input: ${version}`);
+} else {
+  try {
+    const versionJson = fs.readFileSync(versionFilePath);
+    const defaultVersion = JSON.parse(versionJson).latest;
+    version = defaultVersion;
+    console.log(`Using default version from ${versionFilePath}: ${version}`);
+  } catch (err) {
+    console.error(`Error reading version file ${versionFilePath}: ${err.message}`);
+    process.exit(1);
+  }
+}
+
+const url = `https://github.com/typst/typst/releases/download/${version}/typst-x86_64-pc-windows-msvc.zip`;
+const downloadPath = `${process.env.RUNNER_TEMP}/typst.zip`;
+
+await new Promise((resolve, reject) => {
+  const file = fs.createWriteStream(downloadPath);
+  https.get(url, { headers: { Authorization: `token ${token}` } }, (response) => {
+    response.pipe(file);
+    file.on('finish', () => {
+      file.close();
+      resolve();
+    });
+  }).on('error', (err) => {
+    fs.unlink(downloadPath, () => {});
+    reject(err);
+  });
+});
+
+await new Promise((resolve, reject) => {
+  exec(`7z x ${downloadPath} -o${process.env.RUNNER_TEMP}/typst`, (err) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve();
+    }
+  });
+});
+
+const exePath = `${process.env.RUNNER_TEMP}/typst/typst.exe`;
+core.setOutput('path', exePath);
